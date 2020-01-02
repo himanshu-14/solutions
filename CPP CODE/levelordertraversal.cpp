d295aa9e87b714d4b4a71fe7852048424bfe9c47
Himanshu Mittal <himanshu1495@gmail.com>
2020-01-03 00:10:01 +0530

    A christmas tree that can be scaled in size in a jupyter notebook for no reason

--- CPP CODE/levelordertraversal.cpp
+++ CPP CODE/levelordertraversal.cpp
@@ -1,26 +1,31 @@
 //level order traversal of binary tree with queue using stl
-#include<bits/stdc++.h>
-struct Node {
-int data;
-Node* left;
-Node* right;
-};
-void levelOrderTraverse(Node *root){
-if(root==NULL)
-    return;
-queue<Node*> qOfNodes;
-q.push(root);
-while(!q.empty())
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
 {
-    Node* currentNode=q.pop();
-    cout<<currentNode->data;
-    currentNode
+    int data;
+    Node *left;
+    Node *right;
+};
+void levelOrderTraverse(Node *root)
+{
+    if (root == NULL)
+        return;
+    queue<Node *> qOfNodes;
+    qOfNodes.push(root);
+    while (!qOfNodes.empty())
+    {
+        Node *currentNode = qOfNodes.front();
+        qOfNodes.pop();
+        cout << currentNode->data;
+        qOfNodes.push(currentNode->left);
+        qOfNodes.push(currentNode->right);
 
-}
+    }
 }
 
 int main()
 {
 
-return 0;
+    return 0;
 }
--- CPP CODE/matchedBrackets.cpp
+++ CPP CODE/matchedBrackets.cpp
@@ -1,47 +1,47 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-//this function prints all string of given length that have equal opening and closing brackets
-//such that at no point in the string the closing brackets are more than the opening brackets
-int generateBalanced(int length,int numOfOpen,int numOfClose,vector<char> &balancedBrack)
-//use a reference paramter to refer to the vector object to prevent creating copies of the vector
+// this function prints all string of given length that have equal opening and closing brackets
+// such that at no point in the string the closing brackets are more than the opening
+// or or valid bracket
+int generateBalanced(int length, int numOfOpen, int numOfClose, vector<char> &balancedBrack)
+// use a reference paramter to refer to the vector object to prevent creating copies of the vector
 {
-    int currentPos=numOfOpen+numOfClose+1;
-//    cout<<"currentPos"<<currentPos<<endl;
-    if(length%2)//return if length is an odd number
+    int currentPos = numOfOpen + numOfClose + 1;
+    //    cout<<"currentPos"<<currentPos<<endl;
+    if (length % 2) //return if length is an odd number
         return 0;
-    if(numOfOpen+numOfClose==length)//print the vector once required length is reached
+    if (numOfOpen + numOfClose == length) //print the vector once required length is reached
     {
-//        cout<<"printing"<<endl;
-        for(int i=1; i<=length; i++)
-            cout<<balancedBrack[i];
-        cout<<endl;
+        //        cout<<"printing"<<endl;
+        for (int i = 1; i <= length; i++)
+            cout << balancedBrack[i];
+        cout << endl;
         return 1;
     }
-    else if(numOfOpen==(length/2))//once length/2 open brackets are inserted insert only closing brackets
+    else if (numOfOpen == (length / 2)) //once length/2 open brackets are inserted insert only closing brackets
     {
-        balancedBrack[currentPos]=')';
-        generateBalanced(length,numOfOpen,numOfClose+1,balancedBrack);
+        balancedBrack[currentPos] = ')';
+        generateBalanced(length, numOfOpen, numOfClose + 1, balancedBrack);
         return 1;
     }
-    else if(numOfOpen<length/2)//2 choices when open brackets are less than half of the length of the required string
+    else if (numOfOpen < length / 2) //2 choices when open brackets are less than half of the length of the required string
     {
-        balancedBrack[currentPos]='(';
-        generateBalanced(length,numOfOpen+1,numOfClose,balancedBrack);
-        if(numOfClose<numOfOpen)
+        balancedBrack[currentPos] = '(';
+        generateBalanced(length, numOfOpen + 1, numOfClose, balancedBrack);
+        if (numOfClose < numOfOpen)
         {
-            balancedBrack[currentPos]=')';
-            generateBalanced(length,numOfOpen,numOfClose+1,balancedBrack);
+            balancedBrack[currentPos] = ')';
+            generateBalanced(length, numOfOpen, numOfClose + 1, balancedBrack);
         }
         return 1;
     }
-
 }
 int main()
 {
     int length;
-    cin>>length;
-    vector<char> balancedBrack(length+1);//to enable 1 based indexing of the vector
-    generateBalanced(length,0,0,balancedBrack);
+    cin >> length;
+    vector<char> balancedBrack(length + 1); //to enable 1 based indexing of the vector
+    generateBalanced(length, 0, 0, balancedBrack);
     return 0;
 }
